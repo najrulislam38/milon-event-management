6423df88cb983f8326410a7e764f45cb634fb9f9
Najrul <mdnajrulislam.4738@gmail.com>
2023-10-09 08:30:11 +0600

    Update font size of the service title

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,6 +10,9 @@ const Home = () => {
     <>
       <main className="max-w-[1400px] mx-auto px-5 md:px-10 lg:px-20 my-24 ">
         <section data-aos="fade-up" data-aos-duration="800">
+          <h3 className="section-title w-fit mx-auto text-4xl font-poppins font-bold mb-7">
+            Our Services
+          </h3>
           <p className="section-description text-center text-gray-600">
             We handle any type of social event. We are committed to your
             service. <br />
